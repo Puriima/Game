cd98e516c2f24c9231344df3dc4a6bee6982761d
Puriima <purimu@ya.ru>
2024-03-29 12:27:13 +0300

    add all const

--- Main.py
+++ Main.py
@@ -11,14 +11,16 @@ pygame.display.set_caption('Игра')
 icon = pygame.image.load('Image/1612902251_107-p-yabloki-krasnie-fon-162')
 pygame.display.set_icon(icon)
 
-target_image = pygame.image.load(“img/”)
+target_image = pygame.image.load('Image/png-transparent-apple-red-fruit-.png')
 target_width = 80
 target_height = 80
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-target_image = pygame.image.load('Image/png-transparent-apple-red-fruit-.png')
+
 
 color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+
+icon = pygame.image.load('Image/6f59258957e9e6e1abf880bce3db7992f96862ce.jpg')
 
 running = True
 while running:
